31b5f9ef59c03fc4557fe0d5508164f0a5f0bcd5
new <new@new-PC>
2016-02-26 07:04:19 -0500

    ok

--- SIREC-ejb/src/java/ec/sirec/ejb/facade/CatastroPredialPlusvaliaValoracionFacade.java
+++ SIREC-ejb/src/java/ec/sirec/ejb/facade/CatastroPredialPlusvaliaValoracionFacade.java
@@ -125,4 +125,51 @@ public class CatastroPredialPlusvaliaValoracionFacade extends AbstractFacade<Cat
             return lista;
         }
     }
+     
+    public List<Object[]> listaPlusvaliaXParroquia(Object parroquia) {
+        List<Object[]> lista = new ArrayList<Object[]>();                                               
+        String sql = " SELECT  concat(cp.catpre_cod_nacional, cp.catpre_cod_local) clave_catastral,\n" +
+"	a.catprepluval_anio,\n" +
+"	(SELECT cd.catdet_texto parroquia from sirec.catalogo_detalle cd\n" +
+"	where cd.catdet_codigo=cp.catdet_parroquia),\n" +
+"	(SELECT cd.catdet_texto sector from sirec.catalogo_detalle cd\n" +
+"	where cd.catdet_codigo=cp.catdet_sector),\n" +
+"	(select pro.pro_direccion direccion from sirec.propietario pro, sirec.propietario_predio pp\n" +
+"	where pro.pro_ci=pp.pro_ci and pp.catpre_codigo=cp.catpre_codigo),\n" +
+"	(select concat(pro.pro_apellidos,' ',pro.pro_nombres) nombre_propietario from sirec.propietario pro, sirec.propietario_predio pp\n" +
+"	where pro.pro_ci=pp.pro_ci and pp.catpre_codigo=cp.catpre_codigo),\n" +
+"	(select pro.pro_ci ruc_ci from sirec.propietario pro, sirec.propietario_predio pp\n" +
+"	where pro.pro_ci=pp.pro_ci and pp.catpre_codigo=cp.catpre_codigo),\n" +
+" 	(SELECT cd.catdet_texto tipo_tarifa from sirec.catalogo_detalle cd\n" +
+"	where cd.catdet_codigo=a.catdet_tipo_tarifa),\n" +
+"	a.catprepluval_fecha_ultescr,\n" +
+"       a.catprepluval_precioventa,\n" +
+"       a.catprepluval_precioventa_ant,\n" +
+"       a.catprepluval_dif_bruta,\n" +
+"       a.catprepluval_valor_contrmej,\n" +
+"       a.catprepluval_dif_neta,\n" +
+"       a.catprepluval_anios_transf,\n" +
+"       a.catprepluval_anios_transf_val,\n" +
+"       a.catprepluval_dif_final,\n" +
+"       a.catprepluval_porc_rebaja,\n" +
+"       a.catprepluval_valor_rebaja,\n" +
+"       a.catprepluval_baseimp,\n" +
+"       a.catprepluval_tasaproc,\n" +
+"       a.catprepluval_impuesto\n" +
+"  FROM sirec.catastro_predial_plusvalia_valoracion a, sirec.catastro_predial cp\n" +
+"WHERE a.catprepluval_activo=true and\n" +
+"a.catpre_codigo=cp.catpre_codigo and\n" +
+"cp.catdet_parroquia =:parroquia " +
+"order by a.catprepluval_anio, nombre_propietario asc";
+        Query q = getEntityManager().createNativeQuery(sql);
+        q.setParameter("parroquia", parroquia);
+        if (q.getResultList().isEmpty()) {
+            return null;
+        } else {
+            System.out.println(sql);
+            lista = q.getResultList();
+            return lista;
+        }
+    } 
+     
 }
