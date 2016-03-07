2ac5035ff36dcdeac264b97f50ad9f82da1b6ad6
Darwin <Darwin@Darwin-VAIO>
2016-03-06 22:41:46 -0500

    Actualización Darwin

--- SIREC-ejb/src/java/ec/sirec/ejb/servicios/PatenteReporteServicio.java
+++ SIREC-ejb/src/java/ec/sirec/ejb/servicios/PatenteReporteServicio.java
@@ -34,7 +34,33 @@ public class PatenteReporteServicio {
     public List<Object[]> listarDatReporte1(BigDecimal valInicial, BigDecimal valFinal) throws Exception {
         return patenteDao.listReporte1(valInicial, valFinal);
     }
-    public List<Object[]> listaDatReporte2(java.sql.Timestamp fechaInicial,java.sql.Timestamp fechaFinal) throws Exception{
-    return patenteDao.listReporte2(fechaInicial, fechaFinal);
+
+    public List<Object[]> listaDatReporte2(java.sql.Timestamp fechaInicial, java.sql.Timestamp fechaFinal) throws Exception {
+        return patenteDao.listReporte2(fechaInicial, fechaFinal);
     }
-   }
+
+    public List<Object[]> listaDatReporte3(java.sql.Timestamp fechaInicial, java.sql.Timestamp fechaFinal, int codParroquia) throws Exception {
+        return patenteDao.listReporte3(fechaInicial, fechaFinal, codParroquia);
+    }
+
+    public List<Object[]> listaDatReporte4(java.sql.Timestamp fechaInicial, java.sql.Timestamp fechaFinal, int codSector) throws Exception {
+        return patenteDao.listReporte4(fechaInicial, fechaFinal, codSector);
+    }
+
+    public List<Object[]> listaDatReporte5(java.sql.Timestamp fechaInicial, java.sql.Timestamp fechaFinal, String propietario) throws Exception {
+        return patenteDao.listReporte5(fechaInicial, fechaFinal, propietario);
+    }
+
+    public List<Object[]> listaDatReporte6(java.sql.Timestamp fechaInicial, java.sql.Timestamp fechaFinal, int actEco) throws Exception {
+        return patenteDao.listReporte6(fechaInicial, fechaFinal, actEco);
+    }
+    public List<Object[]> listaDatReporte7(java.sql.Timestamp fechaInicial, java.sql.Timestamp fechaFinal) throws Exception {
+        return patenteDao.listReporte7(fechaInicial, fechaFinal);
+    }
+     public List<Object[]> listaDatReporte8(java.sql.Timestamp fechaInicial, java.sql.Timestamp fechaFinal) throws Exception {
+        return patenteDao.listReporte8(fechaInicial, fechaFinal);
+    }
+      public List<Object[]> listaDatReporte9(java.sql.Timestamp fechaInicial, java.sql.Timestamp fechaFinal) throws Exception {
+        return patenteDao.listReporte9(fechaInicial, fechaFinal);
+    }
+}
