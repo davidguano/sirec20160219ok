7d560031e66c532e3045856be43f6471717719a6
new <new@new-PC>
2016-04-14 22:09:07 -0500

    ok

--- SIREC-ejb/src/java/ec/sirec/ejb/servicios/RebajaDesvalorizacionServicio.java
+++ SIREC-ejb/src/java/ec/sirec/ejb/servicios/RebajaDesvalorizacionServicio.java
@@ -9,6 +9,7 @@ package ec.sirec.ejb.servicios;
 
 import ec.sirec.ejb.entidades.RebajaDesvalorizacion;
 import ec.sirec.ejb.facade.RebajaDesvalorizacionFacade;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
@@ -29,4 +30,27 @@ public class RebajaDesvalorizacionServicio {
         return rebajaDesvalorizacionDao.buscarPorCampo(ENTIDAD_REBAJA_DESVALORIZACION, "anio", anio);
     }
 
+     public List<RebajaDesvalorizacion> listarDesvalorizacion() throws Exception {
+        return rebajaDesvalorizacionDao.listarOrdenada(ENTIDAD_REBAJA_DESVALORIZACION, "anio", "desc");
+    }
+        
+    public String crearDesvalorizacion(RebajaDesvalorizacion rebajaDesvalorizacion) throws Exception {
+        rebajaDesvalorizacionDao.crear(rebajaDesvalorizacion);
+        return "Se ha creado la rebajaDesvalorizacion" + rebajaDesvalorizacion.getRebdesCodigo();
+    }
+
+    public String editarDesvalorizacion(RebajaDesvalorizacion rebajaDesvalorizacion) throws Exception {
+        rebajaDesvalorizacionDao.editar(rebajaDesvalorizacion);
+        return "Se ha modificado la aplicación" + rebajaDesvalorizacion.getRebdesCodigo();
+    }
+
+     public String eliminarDesvalorizacion(RebajaDesvalorizacion rebajaDesvalorizacion) throws Exception {
+        rebajaDesvalorizacionDao.eliminarGenerico(ENTIDAD_REBAJA_DESVALORIZACION, "rebdesCodigo", rebajaDesvalorizacion.getRebdesCodigo());
+        return "se ha eliminado el permiso" + rebajaDesvalorizacion.getRebdesCodigo();
+    }
+    
+    public boolean existeAnio(Integer anio) throws Exception {
+        return rebajaDesvalorizacionDao.existePorCampo(ENTIDAD_REBAJA_DESVALORIZACION, "anio", anio);
+    } 
+     
 }
